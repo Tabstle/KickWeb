a2362ef98789c55df9ed4ede76157ebf2fdeeaa4
Tabstle <tabea@stritzke-web.de>
2023-04-06 09:28:14 +0200

    GSAP in Website

--- Sprint3/script.js
+++ Sprint3/script.js
@@ -1,21 +1,42 @@
-let observer = new IntersectionObserver(myObserverCallback, { threshold: [1] });
-
-
-const quote = document.querySelectorAll(".quote");  
-
-quote.forEach((el) => {
-    observer.observe(el);
-});
-
-function myObserverCallback(entries) {
-    entries.forEach((entry) => {
-        if (entry.isIntersecting) {
-            entry.target.style.opacity = 1;
-        } else {
-            entry.target.style.opacity = 0.1;
+gsap.registerPlugin(ScrollTrigger);
+// REVEAL //
+gsap.utils.toArray(".quote").forEach(function (elem) {
+  ScrollTrigger.create({
+    trigger: elem,
+    start: "top 60%",
+    end: "bottom 40%",
+    markers: true,
+    onEnter: function () {
+      gsap.fromTo(
+        elem,
+        { x: 400, autoAlpha: 0 },
+        {
+          duration: 1.25,
+          x: 0,
+          autoAlpha: 1,
+          ease: "back",
+          overwrite: "auto"
         }
-    })
-}
-
-
-console.log(grid)
+      );
+    },
+    onLeave: function () {
+      gsap.fromTo(elem, { autoAlpha: 1 }, { autoAlpha: 0, overwrite: "auto" });
+    },
+    onEnterBack: function () {
+      gsap.fromTo(
+        elem,
+        { x: 400, autoAlpha: 0 },
+        {
+          duration: 1.25,
+          x: 0,
+          autoAlpha: 1,
+          ease: "back",
+          overwrite: "auto"
+        }
+      );
+    },
+    onLeaveBack: function () {
+      gsap.fromTo(elem, { autoAlpha: 1 }, { autoAlpha: 0, overwrite: "auto" });
+    }
+  });
+});

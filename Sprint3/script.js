20a4bf288f2022274423c5e1b0e5c0882582be26
Tabstle <tabea@stritzke-web.de>
2023-04-09 11:12:12 +0200

    Animations Work

--- Sprint3/script.js
+++ Sprint3/script.js
@@ -1,42 +1,53 @@
-gsap.registerPlugin(ScrollTrigger);
-// REVEAL //
-gsap.utils.toArray(".quote").forEach(function (elem) {
-  ScrollTrigger.create({
-    trigger: elem,
-    start: "top 60%",
-    end: "bottom 40%",
-    markers: true,
-    onEnter: function () {
-      gsap.fromTo(
-        elem,
-        { x: 400, autoAlpha: 0 },
-        {
-          duration: 1.25,
-          x: 0,
-          autoAlpha: 1,
-          ease: "back",
-          overwrite: "auto"
-        }
-      );
-    },
-    onLeave: function () {
-      gsap.fromTo(elem, { autoAlpha: 1 }, { autoAlpha: 0, overwrite: "auto" });
-    },
-    onEnterBack: function () {
-      gsap.fromTo(
-        elem,
-        { x: 400, autoAlpha: 0 },
-        {
-          duration: 1.25,
-          x: 0,
-          autoAlpha: 1,
-          ease: "back",
-          overwrite: "auto"
-        }
-      );
-    },
-    onLeaveBack: function () {
-      gsap.fromTo(elem, { autoAlpha: 1 }, { autoAlpha: 0, overwrite: "auto" });
-    }
+function animateFrom(elem, direction) {
+  direction = direction || 1;
+  var x = 0,
+      y = direction * 100;
+  if(elem.classList.contains("animR")) {
+    x = 100;
+    y = 0;
+  } else if (elem.classList.contains("animL")) {
+    x = -100;
+    y = 0;
+  }
+  elem.style.transform = "translate(" + x + "px, " + y + "px)";
+  elem.style.opacity = "0";
+  gsap.fromTo(elem, {x: x, y: y, autoAlpha: 0}, {
+    duration: 1.5, 
+    x: 0,
+    y: 0, 
+    autoAlpha: 1, 
+    ease: "expo", 
+    overwrite: "auto"
+  });
+}
+
+function hide(elem) {
+  gsap.fromTo(elem, { autoAlpha: 1 }, { autoAlpha: 0, overwrite: "auto" });
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+  gsap.registerPlugin(ScrollTrigger);
+  
+  gsap.utils.toArray(".quote").forEach(function(elem) {
+    hide(elem); // assure that the element is hidden when scrolled into view
+    
+    ScrollTrigger.create({
+      trigger: elem,
+      start: "top 70%",
+      end: "bottom 30%",
+      markers: false,
+      onEnter: function() { animateFrom(elem) }, 
+      onEnterBack: function() { animateFrom(elem) },
+      onLeave: function() { hide(elem) }, // assure that the element is hidden when scrolled into view
+      
+      onLeaveBack: function () { hide(elem) }
+
+    });
   });
 });
+
+
+
+
+
+
